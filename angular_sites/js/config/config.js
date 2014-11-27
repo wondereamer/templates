cddd9f8a27f580632aa574d75f4f02eb1cccd81b
Zhouheng.Pan <450000549@qq.com>
2014-11-27 17:07:48 +0800

    11-27
    个人中心安全设置 邮箱 和  密码 修改添加验证

--- angular_sites/js/config/config.js
+++ angular_sites/js/config/config.js
@@ -1,275 +1,274 @@
-var app = angular.module('app', ['ui.router', 'restangular']);
+var app = angular.module("app", ["ui.router", "restangular"]);
 app.config(function($interpolateProvider) {
-	$interpolateProvider.startSymbol('{[');
-	$interpolateProvider.endSymbol(']}');
+	$interpolateProvider.startSymbol("{[");
+	$interpolateProvider.endSymbol("]}");
 });
 
 app.config(function($httpProvider) {
 	// 只在启动时运行一次，所以不能在此设置csrf, 因为此时标志还未被服务器设置。
-	$httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-	$httpProvider.defaults.headers.put['Content-Type'] = 'application/x-www-form-urlencoded';
+	$httpProvider.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+	$httpProvider.defaults.headers.put["Content-Type"] = "application/x-www-form-urlencoded";
 });
 
 app.config(function($stateProvider, $urlRouterProvider) {
-	$urlRouterProvider.otherwise('/index');
+	$urlRouterProvider.otherwise("/index");
 	$stateProvider
-		.state('index', {
-			url: '/index',
+		.state("index", {
+			url: "/index",
 			views: {
-				'': {
-					templateUrl: 'views/home.html',
-					controller: function($scope) {}
-				}
+				"": {
+					templateUrl: "views/home.html",
+				},
 			}
 		})
-		//      发现创意
-		.state('idea', {
-			url: '/idea',
+		//发现创意
+		.state("idea", {
+			url: "/idea",
 			views: {
-				'': {
-					templateUrl: 'views/idea.html'
+				"": {
+					templateUrl: "views/idea.html"
 				},
-				'carousel@idea': {
-					templateUrl: 'views/carousel.html'
+				"carousel@idea": {
+					templateUrl: "views/carousel.html"
 				},
-				'idea-type@idea': {
-					templateUrl: 'views/idea-type.html'
+				"idea-type@idea": {
+					templateUrl: "views/idea-type.html"
 				},
-				'idea-list@idea': {
-					templateUrl: 'views/idea_sites/idea-item.html'
+				"idea-list@idea": {
+					templateUrl: "views/idea_sites/idea-item.html"
 				},
-				'footer@idea': {
-					templateUrl: 'views/footer.html'
+				"footer@idea": {
+					templateUrl: "views/footer.html"
 				}
 			}
 		})
 		//      发起创意
-		.state('submit-idea-center', {
-			url: '/submit-idea-center',
+		.state("submit-idea-center", {
+			url: "/submit-idea-center",
 			views: {
-				'': {
-					templateUrl: 'views/submit-idea-center.html'
+				"": {
+					templateUrl: "views/submit-idea-center.html"
 				},
-				'carousel@submit-idea-center': {
-					templateUrl: 'views/carousel.html'
+				"carousel@submit-idea-center": {
+					templateUrl: "views/carousel.html"
 				},
-				'submit-idea-course@submit-idea-center': {
-					templateUrl: 'views/submit-idea-course.html'
+				"submit-idea-course@submit-idea-center": {
+					templateUrl: "views/submit-idea-course.html"
 				},
-				'footer@submit-idea-center': {
-					templateUrl: 'views/footer.html'
+				"footer@submit-idea-center": {
+					templateUrl: "views/footer.html"
 				}
 			}
 		})
-		.state('submit-idea-center.submit-idea-step1', {
-			url: '/submit-idea-step1',
-			templateUrl: 'views/submit-idea-step1.html'
+		.state("submit-idea-center.submit-idea-step1", {
+			url: "/submit-idea-step1",
+			templateUrl: "views/submit-idea-step1.html"
 		})
-		.state('submit-idea-center.submit-idea-step2', {
-			url: '/submit-idea-step2',
-			templateUrl: 'views/submit-idea-step2.html'
+		.state("submit-idea-center.submit-idea-step2", {
+			url: "/submit-idea-step2",
+			templateUrl: "views/submit-idea-step2.html"
 		})
-		.state('submit-idea-center.submit-idea-step3', {
-			url: '/submit-idea-step3',
-			templateUrl: 'views/submit-idea-step3.html'
+		.state("submit-idea-center.submit-idea-step3", {
+			url: "/submit-idea-step3",
+			templateUrl: "views/submit-idea-step3.html"
 		})
 		//		商店
-		.state('shop', {
-			url: '/shop',
+		.state("shop", {
+			url: "/shop",
 			views: {
-				'': {
-					templateUrl: 'views/shop.html',
-					controller: 'shopController'
+				"": {
+					templateUrl: "views/shop.html",
+					controller: "shopController"
 				},
-				'carousel@shop': {
-					templateUrl: 'views/carousel.html'
+				"carousel@shop": {
+					templateUrl: "views/carousel.html"
 				},
-				'product-list@shop': {
-					templateUrl: 'views/idea_sites/sale-item.html'
+				"product-list@shop": {
+					templateUrl: "views/idea_sites/sale-item.html"
 				},
-				'footer@shop': {
-					templateUrl: 'views/footer.html'
+				"footer@shop": {
+					templateUrl: "views/footer.html"
 				}
 			}
 		})
 		//全部
-		.state('shop.all', {
-			url: '/all',
+		.state("shop.all", {
+			url: "/all",
 			views: {
-				'': {
-					templateUrl: 'views/product-new.html',
+				"": {
+					templateUrl: "views/product-new.html",
 				},
-				'product-filter@shop.all': {
-					templateUrl: 'views/product-filter.html'
+				"product-filter@shop.all": {
+					templateUrl: "views/product-filter.html"
 				},
-				'product-list@shop.all': {
-					templateUrl: 'views/idea_sites/sale-item-new.html'
+				"product-list@shop.all": {
+					templateUrl: "views/idea_sites/sale-item-new.html"
 				}
 			}
 		})
 		//第一分类
-		.state('shop.one', {
-			url: '/one',
+		.state("shop.one", {
+			url: "/one",
 			views: {
-				'': {
-					templateUrl: 'views/product-new.html',
+				"": {
+					templateUrl: "views/product-new.html",
 				},
-				'product-filter@shop.one': {
-					templateUrl: 'views/product-filter.html'
+				"product-filter@shop.one": {
+					templateUrl: "views/product-filter.html"
 				},
-				'product-list@shop.one': {
-					templateUrl: 'views/idea_sites/sale-item-new.html'
+				"product-list@shop.one": {
+					templateUrl: "views/idea_sites/sale-item-new.html"
 				}
 			}
 		})
 		//第二分类
-		.state('shop.two', {
-			url: '/two',
+		.state("shop.two", {
+			url: "/two",
 			views: {
-				'': {
-					templateUrl: 'views/product-new.html',
+				"": {
+					templateUrl: "views/product-new.html",
 				},
-				'product-filter@shop.two': {
-					templateUrl: 'views/product-filter.html'
+				"product-filter@shop.two": {
+					templateUrl: "views/product-filter.html"
 				},
-				'product-list@shop.two': {
-					templateUrl: 'views/idea_sites/sale-item-new.html'
+				"product-list@shop.two": {
+					templateUrl: "views/idea_sites/sale-item-new.html"
 				}
 			}
 		})
 		//第三分类
-		.state('shop.three', {
-			url: '/three',
+		.state("shop.three", {
+			url: "/three",
 			views: {
-				'': {
-					templateUrl: 'views/product-new.html',
+				"": {
+					templateUrl: "views/product-new.html",
 				},
-				'product-filter@shop.three': {
-					templateUrl: 'views/product-filter.html'
+				"product-filter@shop.three": {
+					templateUrl: "views/product-filter.html"
 				},
-				'product-list@shop.three': {
-					templateUrl: 'views/idea_sites/sale-item-new.html'
+				"product-list@shop.three": {
+					templateUrl: "views/idea_sites/sale-item-new.html"
 				}
 			}
 		})
-		.state('shop.product-new', {
-			url: '/product-new',
+		.state("shop.product-new", {
+			url: "/product-new",
 			views: {
-				'': {
-					templateUrl: 'views/product-new.html',
+				"": {
+					templateUrl: "views/product-new.html",
 				},
-				'product-filter@shop.product-new': {
-					templateUrl: 'views/product-filter.html'
+				"product-filter@shop.product-new": {
+					templateUrl: "views/product-filter.html"
 				},
-				'product-list@shop.product-new': {
-					templateUrl: 'views/idea_sites/sale-item-new.html'
+				"product-list@shop.product-new": {
+					templateUrl: "views/idea_sites/sale-item-new.html"
 				}
 			}
 		})
-		.state('shop.product-hot', {
-			url: '/product-hot',
+		.state("shop.product-hot", {
+			url: "/product-hot",
 			views: {
-				'': {
-					templateUrl: 'views/product-hot.html',
+				"": {
+					templateUrl: "views/product-hot.html",
 				},
-				'product-filter@shop.product-hot': {
-					templateUrl: 'views/product-filter.html'
+				"product-filter@shop.product-hot": {
+					templateUrl: "views/product-filter.html"
 				},
-				'product-list@shop.product-hot': {
-					templateUrl: 'views/idea_sites/sale-item-hot.html'
+				"product-list@shop.product-hot": {
+					templateUrl: "views/idea_sites/sale-item-hot.html"
 				}
 			}
 		})
 		//      用户中心
-		.state('user-center', {
-			url: '/user-center',
-			templateUrl: 'views/user-center.html'
+		.state("user-center", {
+			url: "/user-center",
+			templateUrl: "views/user-center.html"
 		})
 		//      用户设置中心
-		.state('user-setting-center', {
-			url: '/user-setting-center',
-			templateUrl: 'views/user-setting-center.html',
-			controller: 'personalController'
+		.state("user-setting-center", {
+			url: "/user-setting-center",
+			templateUrl: "views/user-setting-center.html",
+			controller: "personalController"
 		})
-		.state('user-setting-center.setting', {
-			url: '/setting',
-			templateUrl: 'views/user-setting-list.html'
+		.state("user-setting-center.setting", {
+			url: "/setting",
+			templateUrl: "views/user-setting-list.html"
 		})
-		.state('user-setting-center.info', {
-			url: '/info',
-			templateUrl: 'views/user-info-list.html'
+		.state("user-setting-center.info", {
+			url: "/info",
+			templateUrl: "views/user-info-list.html"
 		})
-		.state('user-setting-center.address', {
-			url: '/address',
-			templateUrl: 'views/user-address-list.html'
+		.state("user-setting-center.address", {
+			url: "/address",
+			templateUrl: "views/user-address-list.html"
 		})
-		.state('user-setting-center.order', {
-			url: '/order',
-			templateUrl: 'views/user-order-list.html'
+		.state("user-setting-center.order", {
+			url: "/order",
+			templateUrl: "views/user-order-list.html"
 		})
-		.state('user-setting-center.coupon', {
-			url: '/coupon',
-			templateUrl: 'views/user-coupon-list.html'
+		.state("user-setting-center.coupon", {
+			url: "/coupon",
+			templateUrl: "views/user-coupon-list.html"
 		})
-		.state('user-setting-center.service', {
-			url: '/service',
-			templateUrl: 'views/user-service-list.html'
+		.state("user-setting-center.service", {
+			url: "/service",
+			templateUrl: "views/user-service-list.html"
 		})
 		//     激活账号
-		.state('activate-account', {
-			url: '/activate-account',
-			templateUrl: 'views/activate-account.html'
+		.state("activate-account", {
+			url: "/activate-account",
+			templateUrl: "views/activate-account.html"
 		})
 		//		商品详情
-		.state('product-detail', {
-			url: '/product-detail',
+		.state("product-detail", {
+			url: "/product-detail",
 			views: {
-				'': {
-					templateUrl: 'views/product-detail.html'
+				"": {
+					templateUrl: "views/product-detail.html"
 				},
-				'footer@product-detail': {
-					templateUrl: 'views/footer.html'
+				"footer@product-detail": {
+					templateUrl: "views/footer.html"
 				}
 			}
 		})
-		.state('product-detail.describe', {
-			url: '/idea-detail-describe',
-			templateUrl: 'views/product-detail-describe.html'
+		.state("product-detail.describe", {
+			url: "/idea-detail-describe",
+			templateUrl: "views/product-detail-describe.html"
 		})
-		.state('product-detail.buy', {
-			url: '/idea-detail-buy',
-			templateUrl: 'views/product-detail-buy.html'
+		.state("product-detail.buy", {
+			url: "/idea-detail-buy",
+			templateUrl: "views/product-detail-buy.html"
 		})
-		.state('product-detail.comment', {
-			url: '/idea-detail-comment',
-			templateUrl: 'views/product-detail-comment.html'
+		.state("product-detail.comment", {
+			url: "/idea-detail-comment",
+			templateUrl: "views/product-detail-comment.html"
 		})
-		.state('product-detail.question', {
-			url: '/idea-detail-question',
-			templateUrl: 'views/product-detail-question.html'
+		.state("product-detail.question", {
+			url: "/idea-detail-question",
+			templateUrl: "views/product-detail-question.html"
 		})
 		//		创意详情
-		.state('idea-detail', {
-			url: '/idea-detail',
+		.state("idea-detail", {
+			url: "/idea-detail",
 			views: {
-				'': {
-					templateUrl: 'views/idea-detail.html'
+				"": {
+					templateUrl: "views/idea-detail.html"
 				},
-				'footer@idea-detail': {
-					templateUrl: 'views/footer.html'
+				"footer@idea-detail": {
+					templateUrl: "views/footer.html"
 				}
 			}
 		})
-		.state('idea-detail.describe', {
-			url: '/idea-detail-describe',
-			templateUrl: 'views/idea-detail-describe.html'
+		.state("idea-detail.describe", {
+			url: "/idea-detail-describe",
+			templateUrl: "views/idea-detail-describe.html"
 		})
-		.state('idea-detail.spit', {
-			url: '/idea-detail-sipt',
-			templateUrl: 'views/idea-detail-spit.html'
+		.state("idea-detail.spit", {
+			url: "/idea-detail-sipt",
+			templateUrl: "views/idea-detail-spit.html"
 		})
-		.state('idea-detail.question', {
-			url: '/idea-detail-question',
-			templateUrl: 'views/idea-detail-question.html'
+		.state("idea-detail.question", {
+			url: "/idea-detail-question",
+			templateUrl: "views/idea-detail-question.html"
 		})
 });
